f12375a544465bd997ace7b06cffc6e5b414ecdd
Kangonga <guchu413@gmail.com>
2023-01-21 19:08:27 +0300

    complete navigation dots v1

---
+++ schemas/testimonials.js
@@ -0,0 +1,33 @@
+export default{
+    name:'testimonials',
+    title:'Testimonials',
+    type:'document',
+    fields: [
+        {
+            name: "name",
+            title: "Name",
+            type: "string",
+        },
+        {
+            name: "company",
+            title: "Company",
+            type: "string",
+        },
+        {
+            name: "imageURL",
+            title: "ImgURL",
+            type: "image",
+            options:{
+                hotspot:true
+            }
+        },
+        {
+            name: "feedback",
+            title: "Feedback",
+            type: "string",
+            options:{
+                hotspot:true
+            }
+        }
+    ]
+}
